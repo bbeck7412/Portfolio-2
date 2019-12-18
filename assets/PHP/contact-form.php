457d97d0943344e91b559f70d18df57dc6b0605d
Brandon M. Beck <55066809+bbeck7412@users.noreply.github.com>
2019-12-18 14:11:41 -0500

    update

--- assets/PHP/contact-form.php
+++ assets/PHP/contact-form.php
@@ -6,9 +6,9 @@ if(isset($_POST['email'])) {
  
     // ADD YOUR EMAIL WHERE YOU WANT TO RECIEVE THE MESSAGES
  
-    $email_to = "Bbeck7412@gmail.com";
+    $email_to = "BBeck7412@gmail.com";
  
-    $email_subject = "Portfolio - Contact Form";
+    $email_subject = "Definity - Contact Form";
  
      
  
